cc84d0f48fd1e6e6519ec0a4063eead246334b22
vikramaditya1190 <63139007+vikramaditya1190@users.noreply.github.com>
2020-06-12 07:50:12 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -17,7 +17,7 @@ async def amireallyalive(alive):
                      f"`My peru owner`: {DEFAULTUSER}\n\n"
                      "`Telethon version:` **6.0.9**\n`Python:` **3.7.4**\n"
                      "`Database Status:` **😀ALL OK**\n\n`Always with you, my master!\n`"
-                     "**Bot Creator:** [🇮🇳INDIAN BHAI](t.me/pureindialover)\n"
-                     "**Co-Owner:** [🇮🇳AKASH](t.me/AKASH_AM1)\n\n"
-                     "     [🇮🇳Deploy This IndianBot🇮🇳](https://github.com/indianbhaiya/IndianBot)") 
+                     "**Bot Creator:** [🇮🇳UTSAV](t.me/OFFICIAL_MADRACER)\n"
+                     "**Co-Owner:** [🇮🇳VIKRAMADITYA](t.me/VIKRAMADITYA1190)\n\n"
+                     "     [🇮🇳POWERFUL USERBOT🇮🇳](https://t.me/aditya1190)") 
 
